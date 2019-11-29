bca15d49c8a738b0754ee101fc1849cb9f445d7c
durban24k <durban24k@users.noreply.github.com>
2019-11-29 22:43:45 +0300

    homejs

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -3,9 +3,14 @@ const api_url = server + '/poll';
 async function query() {
     const response = await fetch(api_url);
     const data = await response.json();
+
     var node = document.createElement("p");
     var textnode = document.createTextNode(data.question);
     node.appendChild(textnode);
     document.getElementById("query").appendChild(node);
+
+    var query = data.responses;
+    var node1 = document.createElement("input").setAttribute("type","radio");
+    var inputnode = document.create
 
 };
