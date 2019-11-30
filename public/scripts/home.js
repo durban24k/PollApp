79e8f7762e0b3c3a2bbef0287b43f366234fa6a3
durban24k <durban24k@users.noreply.github.com>
2019-11-30 09:46:12 +0300

    changes in home.js

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,16 +1,30 @@
 const api_url = server + '/poll';
+let answers;
+
 
 async function query() {
     const response = await fetch(api_url);
     const data = await response.json();
+    answers = data.responses;
+    
 
     var node = document.createElement("p");
     var textnode = document.createTextNode(data.question);
     node.appendChild(textnode);
     document.getElementById("query").appendChild(node);
+    
+    for(o in answers){
+        var input = document.createElement("INPUT");
+        input.setAttribute("type","radio");
+        input.setAttribute("name","responses");
+        input.setAttribute('value', o);
+        var label = document.createElement("Label");
+        label.innerText = o;
+        document.getElementById("radio").appendChild(input);
+        document.getElementById("radio").appendChild(label);
+    }
+};
 
-    var query = data.responses;
-    var node1 = document.createElement("input").setAttribute("type","radio");
-    var inputnode = document.create
-
+window.onLoad = function(){
+    query();
 };
